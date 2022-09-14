588fee8113b401afdd86843533b77343120c3a7b
ester <estera.skana01@gmail.com>
2022-09-14 10:07:47 +0200

    Singleton and Factory

---
+++ src/Factory.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+public class Factory {
+
+    private static Factory factory;
+
+    private Factory() {
+
+    }
+    public static Factory getFactory(){
+        if(factory == null){
+            factory = new Factory();
+        }
+        return factory;
+    }
+    public IPersonReader getReader() throws PersonReaderException {
+
+        Random rand = new Random();
+        if(rand.nextBoolean()){
+            return new DbReader("jdbc:mysql://195.179.236.1:3306/u323045651_citta", "u323045651_java", "P3MYEx!,Q!d!");
+        }
+
+        return new CSVReader(".//resources//list_data.csv");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,13 @@
-import java.io.IOException;
-import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
-
         try {
-            CSVReader readerCSV = new CSVReader(".//resources//list_data.csv");
-            DbReader readerDB = new DbReader("jdbc:mysql://195.179.236.1:3306/u323045651_citta", "u323045651_java", "P3MYEx!,Q!d!");
+            IPersonReader reader = Factory.getFactory().getReader();
+            DoPersonCalculations calculator = new DoPersonCalculations(reader);
 
-            DoPersonCalculations calculatorCSV = new DoPersonCalculations(readerCSV);
-
-            DoPersonCalculations calculatorDb = new DoPersonCalculations(readerDB);
-
-
-            System.out.println("(csv) The number of unique names is: " + calculatorCSV._Names.size());
-            System.out.println("(csv) The number of unique cities is: " + calculatorCSV._Cities.size());
-
-            System.out.println("(db) The number of unique names is: " + calculatorDb._Names.size());
-            System.out.println("(db) The number of unique cities is: " + calculatorDb._Cities.size());
-
-            
+            System.out.println(" The number of unique names is: " + calculator._Names.size());
+            System.out.println(" The number of unique cities is: " + calculator._Cities.size());
         } catch (PersonReaderException exc) {
             System.out.println(exc);
         }
-
     }
-
 }
